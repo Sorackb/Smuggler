61410541d19639684dc3273d1901f808036a3a3a
Sorackb <sorackb@gmail.com>
2016-03-29 11:47:49 -0300

    Improves on bet chance

--- src/main/java/org/lucassouza/smuggler/model/Roulette.java
+++ src/main/java/org/lucassouza/smuggler/model/Roulette.java
@@ -2,6 +2,7 @@ package org.lucassouza.smuggler.model;
 
 import java.util.HashMap;
 import java.util.Set;
+import org.apache.commons.lang3.StringUtils;
 import org.lucassouza.smuggler.type.Color;
 
 /**
@@ -10,26 +11,36 @@ import org.lucassouza.smuggler.type.Color;
  */
 public class Roulette {
 
-  private static final Double MAX_TRY = 8.0; // Número máximo de tentativas
+  private static final Double MAX_TRY = 11.0; // Número máximo de tentativas
   private final HashMap<Color, Bet> bet;
+  private final Boolean applyGrandMartingale;
+  private Integer sequence;
   private Integer initialBet;
-  private Integer balance;
+  private Integer provisionalBalance;
 
-  public Roulette(Integer balance, Color... colors) {
+  public Roulette(Integer initialBalance, Color... colors) {
+    this(initialBalance, false, colors);
+  }
+
+  public Roulette(Integer initialBalance, Boolean applyGrandMartingale, Color... colors) {
     this.bet = new HashMap<>();
-    this.balance = balance;
+    this.sequence = 0;
+    this.provisionalBalance = initialBalance;
+    this.applyGrandMartingale = applyGrandMartingale;
     this.recalculateInitialBet();
 
     for (Color color : colors) {
       Bet newBet = new Bet();
 
-      newBet.setInitialBet(this.initialBet);
-      newBet.setNextBet(this.initialBet);
+      newBet.setBind(this.initialBet);
       this.bet.put(color, newBet);
     }
   }
 
   public void roll(Color color) {
+    Integer difference = 0;
+    Boolean dangerous = false;
+    Boolean stopped = false;
     String summary = "";
     Set<Color> keys;
 
@@ -37,52 +48,74 @@ public class Roulette {
     keys = this.bet.keySet();
 
     for (Color key : keys) {
-      Bet actualBet = this.bet.get(key);
+      Bet actualBet;
+      Integer nextBind;
+
+      actualBet = this.bet.get(key);
+      nextBind = actualBet.getBind() * 2;
+
+      // Aplica a estratégia Grand Martingale que adiciona o valor da aposta a cada nova tentativa
+      if (this.applyGrandMartingale) {
+        nextBind = nextBind + this.initialBet;
+      }
 
       if (key.equals(color)) {
-        this.setBalance(this.balance + actualBet.getInitialBet());
-        actualBet.setInitialBet(this.initialBet);
-        actualBet.setNextBet(this.initialBet);
+        this.setProvisionalBalance(this.provisionalBalance + actualBet.getTotal());
+        actualBet.setBind(this.initialBet);
+      } else if (this.provisionalBalance - nextBind < 0) { // Evita valores negativos parando a aposta
+      //} else if (this.provisionalBalance - nextBind < nextBind) { // Evita valores negativos parando a aposta
+        stopped = true;
+        actualBet.setBind(this.initialBet);
       } else {
-        actualBet.setNextBet(actualBet.getNextBet() * 2);
+        this.setProvisionalBalance(this.provisionalBalance - nextBind);
+        actualBet.setBind(nextBind);
       }
+
+      difference = difference + actualBet.getBind();
 
       if (!summary.isEmpty()) {
         summary = summary + " / ";
       }
 
-      summary = summary + key.name() + ": " + actualBet.getNextBet();
-      
-      if (actualBet.getNextBet() > this.balance) {
-        summary = summary + " <= LEAK";
+      summary = summary + key.name() + ": " + actualBet.getBind();
+
+      if (actualBet.getBind() > this.provisionalBalance) {
+        dangerous = true;
       }
     }
-    
+
+    if (stopped) {
+      summary = summary + " <= STOPPED";
+    } else if (dangerous) {
+      summary = summary + " <= DANGER";
+    }
+
     System.out.println(summary);
-    System.out.println("Balance: " + this.balance);
-    System.out.println("======================");
+    System.out.println("Balance: " + this.provisionalBalance + " / " + (this.provisionalBalance + difference));
+    this.sequence = this.sequence + 1;
+    System.out.println("=======" + StringUtils.center(this.sequence.toString(), 5) + "=======");
   }
 
   public HashMap<Color, Bet> getBet() {
     return this.bet;
   }
 
-  public void setBalance(Integer balance) {
-    this.balance = balance;
+  public void setProvisionalBalance(Integer provisionalBalance) {
+    this.provisionalBalance = provisionalBalance;
     this.recalculateInitialBet();
   }
 
-  public Integer getBalance() {
-    return this.balance;
+  public Integer getProvisionalBalance() {
+    return this.provisionalBalance;
   }
-  
+
   private void recalculateInitialBet() {
     Integer newInitialBet;
     Double root;
 
-    root = this.balance / Math.pow(2, MAX_TRY);
+    root = this.provisionalBalance / Math.pow(2, MAX_TRY);
     newInitialBet = root.intValue();
-    
+
     if (newInitialBet == 0) {
       newInitialBet = 1;
     }
--- src/main/java/org/lucassouza/smuggler/view/Test.java
+++ src/main/java/org/lucassouza/smuggler/view/Test.java
@@ -4,10 +4,10 @@ import java.util.HashMap;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 import org.lucassouza.smuggler.model.Bet;
-import org.lucassouza.smuggler.model.CSGODouble;
 import org.lucassouza.smuggler.model.Roulette;
 import org.lucassouza.smuggler.type.Color;
 import static org.lucassouza.smuggler.type.Color.BLACK;
+import static org.lucassouza.smuggler.type.Color.GREEN;
 import static org.lucassouza.smuggler.type.Color.RED;
 
 /**
@@ -15,39 +15,53 @@ import static org.lucassouza.smuggler.type.Color.RED;
  * @author lucas.souza
  */
 public class Test {
+
   private static Roulette roulette;
 
   public static void main(String[] args) {
     //CSGODouble csGoDouble = new CSGODouble();
-    roulette = new Roulette(500, BLACK, RED);
+    roulette = new Roulette(10000, true, BLACK, RED);
     HashMap<Color, Bet> bet = roulette.getBet();
 
-    System.out.println("BLACK: " + bet.get(BLACK).getNextBet() + " / RED: " + bet.get(RED).getNextBet());
-    System.out.println("Balanço: " + roulette.getBalance());
-    System.out.println("======================");
+    System.out.println("BLACK: " + bet.get(BLACK).getBind() + " / RED: " + bet.get(RED).getBind());
+    System.out.println("Balance: " + roulette.getProvisionalBalance());
+    System.out.println("=======  0  =======");
 
-    for (int i = 0; i < 2880; i++) {
+    for (int i = 0; i < 960; i++) {
       execute();
     }
   }
-  
-  private static void execute() {
-    Random random = new Random();
-    //float chance = random.nextFloat();
-    Integer chance2 = ThreadLocalRandom.current().nextInt(1, 2 + 1);
-    
-    if (chance2 == 1) {
-      roulette.roll(RED);
-    } else {
-      roulette.roll(BLACK);
-    }
 
-    /*if (chance <= 0.06f) {
+  private static void execute() {
+    Boolean accurate = true;
+
+    if (accurate) {
+      runAccurate();
+    } else {
+      runNotAccurate();
+    }
+  }
+
+  private static void runAccurate() {
+    Random random = new Random();
+    float chance = random.nextFloat();
+
+    if (chance <= 0.06f) {
       roulette.roll(GREEN);
     } else if (chance <= 0.53f) {
       roulette.roll(RED);
     } else {
       roulette.roll(BLACK);
-    }*/
+    }
+  }
+
+  private static void runNotAccurate() {
+    Integer chance2 = ThreadLocalRandom.current().nextInt(1, 2 + 1);
+
+    if (chance2 == 1) {
+      roulette.roll(RED);
+    } else {
+      roulette.roll(BLACK);
+    }
   }
 }
